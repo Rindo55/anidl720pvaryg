991eb82d03afeebe2ba165cccc1ed1c1475c2087
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-04-14 04:31:57 +0530

    Update usschedule.py

--- main/modules/usschedule.py
+++ main/modules/usschedule.py
@@ -5,8 +5,8 @@ from main.inline import button2
 from pyrogram.types import Message
 from config import STATUS_ID, INDEX_ID, UPLOADS_ID, SCHEDULE_ID, UPLOADS_USERNAME
 
-GHOST_ID =  -1001373634390
-US_ID = 19
+GHOST_ID =  -1001895203720
+US_ID = 13939
 schedulex = app.get_messages(GHOST_ID,US_ID)
 schedulex: Message
 
